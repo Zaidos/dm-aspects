12860426c0bbc00df92b172a2842b86c04a02400
Alven Diaz <nevlad@gmail.com>
2014-05-09 01:30:20 -0500

    Extending documentation for BSON ID aspect.

--- lib/datamapper/aspects/bson_id.rb
+++ lib/datamapper/aspects/bson_id.rb
@@ -2,10 +2,22 @@
 
 module DataMapper
   module Aspects
+    # Public: Provides a valid BSON Object ID key property.
+    # Also validates that BSON IDs are valid object ids.
+    #
+    # Examples
+    #
+    #   my_obj.id.id_is_valid?
+    #   # => true
+    #
+    #   my_obj.id.id_generation_time
+    #   # => 2014-05-09 06:07:00 UTC
     module BSONID
-
       def self.included(base)
+        # Public: Provides the default aspect's attributes.
         base.property :id, String, length: 24, key: true, default: Moped::BSON::ObjectId.new.to_s
+
+        # Internal: Validates that the BSON ID is a valid object id.
         base.validates_with_method :id, method: :id_is_valid?
 
         # Public: Retrieves the generation time of the BSON Object ID.
